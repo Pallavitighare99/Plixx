de7976a0fbb94332fdc8a66108cec8121a5e369d
Pallavitighare99 <pallavitighare01@gmail.com>
2023-03-12 19:45:16 +0530

    Contact-us

--- src/screen/ContactUs.js
+++ src/screen/ContactUs.js
@@ -2,55 +2,56 @@ import React from 'react'
 import Layout from './../layout/Layout'
 import Head from '../components/Head'
 import { FiMail, FiPhoneCall, FiMapPin } from 'react-icons/fi'
+import { InlineError } from '../components/Notifications/Error'
+import { Input } from '../components/UsedInput'
 
 
 function contactUs() {
-  const contactData = [
-    {
-      id: 1,
-      title: 'Email Us',
-      info: 'Interactive grow backend ideas for cross-platform models.',
-      icon: FiMail,
-      contact: "pallavitighare01@gmail.com",
-    },
-    {
-      id: 2,
-      title: 'Call Us',
-      info: 'Interactive grow backend ideas for cross-platform models.',
-      icon: FiPhoneCall,
-      contact: "+91 9644377621",
-    },
-    {
-      id: 3,
-      title: 'Location',
-      info: 'At-Post Mathani, Tah. Mouda, Dist. Nagpur-441104',
-      icon: FiMapPin,
-      contact: "",
-    },
-  ]
+
   return (
     <Layout>
-      <div className='min-height-screen container mx-auto px-2 my-6'>
-        <Head title="Contact Us" />
-        <div className='grid mg:grid-cols-2 gap-6 lg:my-20 my-10 lg:grid-cols-3 xl:gap-8'>
-          {
-            contactData.map((item) => (
-              <div className='border border-border flex flex-col justify-center items-center p-10 bg-dry rounded-lg text-center' key={item.id}>
-               <span className='flex flex-col justify-center items-center w-20 h-20 mb-3 rounded-full bg-main text-subMain text-2xl'>
-                  <item.icon />
-               </span>
-                <h5 className='text-xl font-semibold mb-2'>
-                  {item.title}
-                </h5>
-                <p className='mb-0 text-sm text-text leading-7'>
-                 <a href={`mailto:${item.contact}`} className='text-blue-600'>{item.contact}</a>
-                {' '}
-                {item.info}
-                </p>
-              </div>
-            ))
-          }
-        </div>
+      <Head title="24/7 Access To All Premium Users" />
+      <div className='container mx-auto px-2 my-24 flex flex-col justify-center items-center '>
+        <form className='w-full 2xl:w-2/5 gap-8 p-8 sm:p-14 md:w-3/5 flex flex-col justify-center items-center bg-dry rounded-lg border border-border'>
+          <h2 className='text-2xl font-bold'>Having Queries??</h2>
+          <div className='w-full'>
+            <Input
+              label="Previous Password"
+              placeholder="*******"
+              type="password"
+              name="oldPassword"
+              bg={true}
+            />
+
+          </div>
+          <div className='w-full'>
+            <Input
+              label=""
+              placeholder="*******"
+              type="password"
+              name="oldPassword"
+              bg={true}
+            />
+
+          </div>
+          <div className='w-full'>
+            <Input
+              label="Previous Password"
+              placeholder="*******"
+              type="password"
+              name="oldPassword"
+              bg={true}
+            />
+
+          </div>
+          <div
+            type='submit'
+            className='flex justify-end items-center my-4'>
+            <button className='bg-main transitions hover:bg-subMain border border-subMain text-white py-3 px-6 rounded w-full sm:w-auto'>
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </Layout>
   )
--- src/screen/PremiumScreen.js
+++ src/screen/PremiumScreen.js
@@ -1,68 +1,74 @@
 import React from 'react';
 import Layout from './../layout/Layout';
 import Head from '../components/Head';
-import { FiMail, FiPhoneCall, FiMapPin } from 'react-icons/fi';
 
 function SubscriptionPage() {
-  const monthlyPlan = {
-    name: 'Monthly Plan',
-    price: '₹99 / month',
-    benefits: [
-      'Access to all premium features for 30 days',
-      '24/7 customer support',
-    ],
-  };
 
-  const yearlyPlan = {
-    name: 'Yearly Plan',
-    price: '₹499 / year',
-    benefits: [
-      'Access to all premium features for 365 days',
-      '24/7 customer support',
-    ],
-  };
+  const subData = [
+    {
+      id: 1,
+      name: 'Monthly Plan',
+      price: '₹99 / month',
+      benefits: [
+        'Access to all premium features for 30 days',
+        '24/7 customer support',
+      ],
+    },
+   
+    {
+      id: 2,
+      name: 'Yearly Plan',
+      price: '₹499 / year',
+      benefits: [
+        'Access to all premium features for 365 days',
+        '24/7 customer support',
+      ],
+    },
+    {
+      id: 3,
+      name: 'LifeTime Plan',
+      price: '₹1499 / Life',
+      benefits: [
+        'Access to all premium features for 365 days',
+        '24/7 customer support',
+      ],
+    },
+
+  ]
+
 
   return (
     <Layout>
-      <div className='min-h-screen container mx-auto px-2 my-6'>
-        <Head title='Subscribe to our service' />
-        <div className='grid grid-cols-1 gap-4 lg:grid-cols-2 xl:gap-8 pt-10'>
-          <div className='max-w-xs border border-gray-300 rounded-lg shadow-md overflow-hidden text-sm'>
-            <div className='bg-subMain py-2 px-4'>
-              <h2 className='text-md font-bold'>{monthlyPlan.name}</h2>
-              <p className='text-sm font-bold my-2'>{monthlyPlan.price}</p>
-            </div>
-            <div className='p-4'>
-              <ul className='list-disc list-inside my-2'>
-                {monthlyPlan.benefits.map((benefit) => (
+      <div className='min-height-screen container mx-auto px-2 my-6'>
+        <Head title="Subscribe to our service" />
+        <div className='grid mg:grid-cols-1 lg:grid-cols-3 gap-6 lg:my-20 my-10 xl:gap-10'>
+          {
+            subData.map((item) => (
+              <div className='border border-border flex flex-col justify-center items-center p-5 bg-dry rounded-lg text-center hover:bg-main' key={item.id}>
+                <span className='flex flex-col justify-center items-center w-80 h-20 mb-3  bg-subMain text-white text-xl'>
+                  {item.name}
+                </span>
+                <span className='flex flex-col justify-center items-center w-60 h-20  bg-main text-subMain text-xl'>
+                  {item.price}
+                </span>
+                <h5 className='text-xl font-semibold mb-2'>
+                  {item.title}
+                </h5>
+                <ul className='mb-0 text-sm text-text leading-7'>
+                {item.benefits.map((benefit) => (
                   <li key={benefit} className='my-2 text-gray-600'>
                     {benefit}
                   </li>
                 ))}
               </ul>
-              <button className='bg-blue-500 text-white py-2 px-3 rounded-lg text-sm'>
-                Subscribe
+
+              {/* <button className='bg-subMain transitions hover:bg-main border border-subMain text-white py-3 px-6 rounded lg:rounded-full sm:w-auto'> */}
+               <button className='bg-white flex flex-row justify-center items-center gap-4 text-black p-4 rounded-lg w-full hover:bg-subMain hover:text-white'>
+                Subscribe {item.name}
               </button>
-            </div>
-          </div>
-          <div className='max-w-xs border border-gray-300 rounded-lg shadow-md overflow-hidden text-sm'>
-            <div className='bg-subMain py-2 px-4'>
-              <h2 className='text-md font-bold'>{yearlyPlan.name}</h2>
-              <p className='text-sm font-bold my-2'>{yearlyPlan.price}</p>
-            </div>
-            <div className='p-4'>
-              <ul className='list-disc list-inside my-2'>
-                {yearlyPlan.benefits.map((benefit) => (
-                  <li key={benefit} className='my-2 text-gray-600'>
-                    {benefit}
-                  </li>
-                ))}
-              </ul>
-              <button className='bg-blue-500 text-white py-2 px-3 rounded-lg text-sm'>
-                Subscribe
-              </button>
-            </div>
-          </div>
+              </div>
+            ))
+          }
         </div>
       </div>
     </Layout>
